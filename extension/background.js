b6ec5995bee32f92f788f683f805542a51b13197
David <66833306+sk3tchDavid@users.noreply.github.com>
2023-10-28 22:30:48 +0200

    big update and working again

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,11 @@
-function removeCssSelector() {
+setInterval(function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const tab = tabs[0];
+      const currentUrl = tab.url;
+      if (currentUrl.includes("watch?v=")) {
+        const newUrl = currentUrl.replace(/watch\?v=/g, "embed/");
+        chrome.tabs.update(tab.id, { url: newUrl });
+      }
+    });
+  }, 1000); //1s
   
-    const element = document.querySelector('tp-yt-paper-dialog.style-scope');
-  
-    if (element) {
-      element.remove();
-    }
-  }
-  setInterval(removeCssSelector, 100);
