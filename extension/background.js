34d464cabdd63953295b4378d41488ca3cb5058e
David <66833306+sk3tchDavid@users.noreply.github.com>
2023-10-28 23:29:46 +0200

    update

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,10 @@
 setInterval(function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      const currentUrl = tab.url;
-      if (currentUrl.includes("watch?v=")) {
-        const newUrl = currentUrl.replace(/watch\?v=/g, "embed/");
-        chrome.tabs.update(tab.id, { url: newUrl });
-      }
-    });
-  }, 1000); //1s
-  
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tab = tabs[0];
+    const currentUrl = tab.url;
+    if (currentUrl.includes("watch?v=")) {
+      const newUrl = currentUrl.replace(/watch\?v=/g, "embed/");
+      chrome.tabs.create({ url: newUrl });
+    }
+  });
+}, 1000); //1s
